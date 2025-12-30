ce0937878180414a3fd9376b35b0352ab38d2b84
Kelvin <kelvin@futo.org>
2025-12-30 17:06:15 +0100

    Fix ignore original for downloads

--- Grayjay.ClientServer/Helpers/VideoHelper.cs
+++ Grayjay.ClientServer/Helpers/VideoHelper.cs
@@ -101,7 +101,7 @@ namespace Grayjay.ClientServer.Helpers
             if (hasPriority)
                 sources = sources.Where(x => x.Priority).ToList();
             var hasOriginal = sources.Any(x => x.Original);
-            if (hasOriginal && GrayjaySettings.Instance.Playback.PreferOriginalAudio)
+            if (hasOriginal && GrayjaySettings.Instance.Playback.PreferOriginalAudio && !ignoreOriginal)
                 sources = sources.Where(x => x.Original).ToList();
 
             var languageToFilter = (prefLanguage != null && sources.Any(x => x.Language == prefLanguage) 
