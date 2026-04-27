2b59029c6d01e446428e15ac7baa709db19e9b23
guthubrx <githubrx@runbox.com>
2026-04-27 20:44:42 +0200

    feat(player): collapsible carousels with unified toggle bar in theater mode

--- Grayjay.Desktop.Web/src/components/contentDetails/VideoDetailView/index.tsx
+++ Grayjay.Desktop.Web/src/components/contentDetails/VideoDetailView/index.tsx
@@ -84,6 +84,7 @@ import VideoThumbnailView from "../../content/VideoThumbnailView";
 import { IPlatformVideo } from "../../../backend/models/content/IPlatformVideo";
 import HorizontalScrollContainer from "../../containers/HorizontalScrollContainer";
 import HorizontalFlexibleArrayList from "../../containers/HorizontalFlexibleArrayList";
+import iconChevronDown from '../../../assets/icons/icon16_chevron_down.svg';
 import SideBar from "../../menus/SideBar";
 import LiveChatWindow from "../../LiveChatWindow";
 import { focusScope } from '../../../focusScope'; void focusScope;
@@ -121,6 +122,7 @@ interface HorizontalCarouselProps {
     onScrollEnd?: () => void;
     builder: (index: Accessor<number | undefined>, item: Accessor<any>) => any;
     flex?: string;
+    onTitleClick?: () => void;
 }
 
 const HorizontalCarousel: Component<HorizontalCarouselProps> = (props) => {
@@ -151,7 +153,13 @@ const HorizontalCarousel: Component<HorizontalCarouselProps> = (props) => {
             onMouseEnter={() => { setHover(true); update(); }}
             onMouseLeave={() => setHover(false)}
         >
-            <div class={styles.carouselSectionTitle}>{props.title}</div>
+            <div
+                class={styles.carouselSectionTitle}
+                style={{ cursor: props.onTitleClick ? "pointer" : undefined, "user-select": "none" }}
+                onClick={() => props.onTitleClick?.()}
+            >
+                {props.title}
+            </div>
             <div style={{ width: "100%", padding: "8px 40px 0 40px", "box-sizing": "border-box" }}>
                 <HorizontalScrollContainer ref={scrollRef} subtle={true}>
                     <HorizontalFlexibleArrayList
@@ -256,6 +264,13 @@ const VideoDetailView: Component<VideoDetailsProps> = (props) => {
             return undefined;
         return await HistoryBackend.historyPager();
     });
+    const [carouselsCollapsed$, setCarouselsCollapsed] = createSignal(false);
+    SettingsBackend.persistGet("carouselsCollapsed", false).then((r: boolean) => setCarouselsCollapsed(r)).catch(e => console.error("Failed to get persistent setting 'carouselsCollapsed'.", e));
+    const toggleCarouselsCollapsed = () => {
+        const n = !carouselsCollapsed$();
+        setCarouselsCollapsed(n);
+        SettingsBackend.persistSet("carouselsCollapsed", n);
+    };
     const continueWatchingItems$ = createMemo(() => {
         const items = historyPager$()?.data ?? [];
         const currentUrl = videoLoaded$()?.url;
@@ -1702,6 +1717,16 @@ const VideoDetailView: Component<VideoDetailsProps> = (props) => {
                     </div>
                 </StickyShrinkOnScrollContainer>
                 <Show when={anyHorizontalCarousel$()}>
+                    <div
+                        class={styles.carouselsToggleBar}
+                        onClick={toggleCarouselsCollapsed}
+                        title={carouselsCollapsed$() ? "Show carousels" : "Hide carousels"}
+                        use:focusable={{ onPress: toggleCarouselsCollapsed }}
+                    >
+                        <img src={iconChevronDown} class={styles.carouselsToggleChevron} style={{ transform: carouselsCollapsed$() ? "rotate(-90deg)" : undefined }} />
+                    </div>
+                </Show>
+                <Show when={anyHorizontalCarousel$() && !carouselsCollapsed$()}>
                     <div style={{
                         display: "flex",
                         "flex-direction": carouselsSideBySide$() ? "row" : "column",
@@ -1712,6 +1737,7 @@ const VideoDetailView: Component<VideoDetailsProps> = (props) => {
                         <Show when={showHorizontalQueue$()}>
                             <HorizontalCarousel
                                 title="Queue"
+                                onTitleClick={toggleCarouselsCollapsed}
                                 items={video?.queue() ?? []}
                                 flex={carouselsSideBySide$() ? `0 1 ${Math.min(video?.queue()?.length ?? 0, 6) * 166 + 80}px` : undefined}
                                 builder={(_, item) => (
@@ -1734,6 +1760,7 @@ const VideoDetailView: Component<VideoDetailsProps> = (props) => {
                         <Show when={showHorizontalContinueWatching$()}>
                             <HorizontalCarousel
                                 title="Continue Watching"
+                                onTitleClick={toggleCarouselsCollapsed}
                                 items={continueWatchingItems$()}
                                 flex={carouselsSideBySide$() ? `0 1 ${Math.min(continueWatchingItems$()?.length ?? 0, 6) * 166 + 80}px` : undefined}
                                 builder={(_, item) => (
@@ -1754,6 +1781,7 @@ const VideoDetailView: Component<VideoDetailsProps> = (props) => {
                         <Show when={showHorizontalRecommendations$()}>
                             <HorizontalCarousel
                                 title="Recommendations"
+                                onTitleClick={toggleCarouselsCollapsed}
                                 pager={recomPager$()}
                                 onScrollEnd={onScrollEndRecommendations}
                                 flex={carouselsSideBySide$() ? "1 0 412px" : undefined}
@@ -1827,7 +1855,7 @@ const VideoDetailView: Component<VideoDetailsProps> = (props) => {
                                     </Show>
                                 </div>
 
-                                <SubscribeButton author={author$()?.url} style={{"margin-top": "29px"}} focusable={true} />
+                                <SubscribeButton author={author$()?.url} style={{"margin-top": "22px"}} focusable={true} />
 
                                 <div style="flex-grow: 1;">
                                 </div>
